4708d0fa43020238f376c575c718bc3f3ff94e20
nullforce <glenngit@nullforce.com>
2019-11-18 00:37:27 -0600

    Add the prunedms command
    Closes #9

---
+++ src/commands/prunedms.ts
@@ -0,0 +1,47 @@
+import { Command } from "discord-akairo";
+import { Message } from "discord.js";
+import { DMChannel } from "discord.js";
+
+class PingCommand extends Command {
+    constructor() {
+        super("prunedms", {
+            aliases: ["prunedms"],
+            args: [
+                {
+                    default: "1",
+                    id: "amount",
+                    type: "number",
+                },
+            ],
+            channelRestriction: "dm",
+        });
+    }
+
+    public async exec(message: Message, args: any) {
+        const amount = Math.min(args.amount, 10);
+        const channel = message.channel as DMChannel;
+        let deletedMessages = 0;
+        let fetchSize = 50;
+
+        while (deletedMessages < amount) {
+            const messages = await channel.fetchMessages({
+                limit: fetchSize,
+            });
+
+            const myMessages = messages
+            .filter((m) => m.deletable || m.author.id === message.client.user.id)
+            .sort((a, b) => a.createdTimestamp < b.createdTimestamp ? 1 : -1);
+
+            for (const msg of myMessages.values()) {
+                await msg.delete();
+                deletedMessages++;
+
+                if (deletedMessages >= amount) break;
+            }
+
+            fetchSize = fetchSize + 50;
+        }
+    }
+}
+
+module.exports = PingCommand;
